288e6356afaa1cad9d14947da742226c74f4080b
furkanDts <furkan.dts99@gmail.com>
2025-06-24 23:29:46 +0300

    feat: setup tailwind and shadcnUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from "./components/ui/button"
+
 function App() {
     return (
         <>
-            <h1>hello, world!</h1>
+            <div className="p-5 flex justify-center">
+                <Button>ShadcnUI Button</Button>
+            </div>
         </>
     )
 }
